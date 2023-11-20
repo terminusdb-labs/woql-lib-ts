f935767a6a31be97bae92fb7ab38dc19c96d4f94
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-11-20 11:32:45 +0100

    Just exports syntax.ts symbols in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { triple, letvars, and } from './syntax.js';
-
-const query = letvars((x, y, z) => and(triple(x, y, z), triple(z, y, z)))
-console.log(JSON.stringify(query))
-
+export * from './syntax.js';
